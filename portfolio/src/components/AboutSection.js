7ab9103e5c27f084ddd9a78b7ba6d285789359e0
manuelpapa <manulito@de-vio.com>
2020-12-14 16:51:14 +0100

    Style About and Service section.

--- portfolio/src/components/AboutSection.js
+++ portfolio/src/components/AboutSection.js
@@ -1,10 +1,11 @@
 import React from "react";
 import home1 from "../img/home1.png";
-import styled from "styled-components";
+// import styled from "styled-components";
+import { Layout, Description, Hide, Image } from "../styles";
 
 const AboutSection = () => {
   return (
-    <About>
+    <Layout>
       <Description>
         <div className="title">
           <Hide>
@@ -28,39 +29,8 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="guy with a cam" />
       </Image>
-    </About>
+    </Layout>
   );
 };
-
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 60vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
